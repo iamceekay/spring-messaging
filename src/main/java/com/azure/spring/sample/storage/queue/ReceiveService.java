d4cb8ae370eb96d97493a110884a195f066498e1
sneha <samantaray.snehamayee@gmail.com>
2023-05-04 12:37:08 +0530

    Spring messaging queue changes

---
+++ src/main/java/com/azure/spring/sample/storage/queue/ReceiveService.java
@@ -0,0 +1,46 @@
+package com.azure.spring.sample.storage.queue;
+
+import com.azure.spring.integration.storage.queue.inbound.StorageQueueMessageSource;
+import com.azure.spring.messaging.AzureHeaders;
+import com.azure.spring.messaging.checkpoint.Checkpointer;
+import com.azure.spring.messaging.storage.queue.core.StorageQueueTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReceiveService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReceiveService.class);
+    
+    private static final String STORAGE_QUEUE_NAME = "storagejavacase2";
+    private static final String INPUT_CHANNEL = "input";
+
+    @Bean
+    @InboundChannelAdapter(channel = INPUT_CHANNEL, poller = @Poller(fixedDelay = "1000"))
+    public StorageQueueMessageSource storageQueueMessageSource(StorageQueueTemplate storageQueueTemplate) {
+
+        return new StorageQueueMessageSource(STORAGE_QUEUE_NAME, storageQueueTemplate);
+    }
+
+    /**
+     * This message receiver binding with {@link StorageQueueMessageSource}
+     * via {@link MessageChannel} has name {@value INPUT_CHANNEL}
+     */
+    @ServiceActivator(inputChannel = INPUT_CHANNEL)
+    public void messageReceiver(byte[] payload, @Header(AzureHeaders.CHECKPOINTER) Checkpointer checkpointer) {
+        String message = new String(payload);
+        LOGGER.info("New message received: '{}'", message);
+        checkpointer.success()
+            .doOnError(Throwable::printStackTrace)
+            .doOnSuccess(t -> LOGGER.info("Message '{}' successfully checkpointed", message))
+            .block();
+
+    }
+}
